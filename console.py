2f8b0d305f4fd669a918fc3c2cde5a161dc2a8c8
Francis Thore <thorefrancis@gmail.com>
2024-02-20 22:09:41 +0200

    undo

--- console.py
+++ console.py
@@ -112,22 +112,19 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overrides the emptyline method of CMD """
         pass
- 
- 
+
     def do_create(self, args):
         """ Create an object of any class"""
-        # if not args:
-        #     print("** class name missing **")
-        #     return
-        # elif args not in HBNBCommand.classes:
-        #     print("** class doesn't exist **")
-        #     return
-        print(args in HBNBCommand.classes)
-        """
+        if not args:
+            print("** class name missing **")
+            return
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
         new_instance = HBNBCommand.classes[args]()
         storage.save()
         print(new_instance.id)
-        storage.save() """
+        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
