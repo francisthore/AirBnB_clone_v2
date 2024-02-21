6f649904a3467e9735c6559f554e928fae0ec72a
precious-appiah <appiahprecious99@gmail.co>
2024-02-21 16:31:55 +0000

    task fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
 -- script that prepares dbs, create a user and grant users permission
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+CREATE DATABASE IF NOT EXISTS performance_schema;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
 
 GRANT  ALL PRIVILEGES on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
--- script that prepares dbs, create a user and grant users permission
+-- script that prepares dbs, create a user and grant users permission1
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE DATABASE IF NOT EXISTS performance_schema;
 
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_test_pwd';
 
