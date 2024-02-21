16337b61359002d1c830fe0742ffb46220f590cd
Francis Thore <thorefrancis@gmail.com>
2024-02-21 12:03:51 -0800

    fix: sql test setup errors

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,12 @@
 -- script that prepares dbs, create a user and grant users permission1
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE DATABASE IF NOT EXISTS performance_schema;
+USE hbnb_test_db;
 
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES on hbnb_test_db.* TO 'hbnb_test'@'hbnb_test';
+GRANT ALL PRIVILEGES ON  hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
