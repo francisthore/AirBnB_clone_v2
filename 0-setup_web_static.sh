993fe705ab85e4b65dbdbdaf98b49b41fa28fa71
Francis Thore <thorefrancis@gmail.com>
2024-04-03 22:42:39 +0200

    setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+# sets up server for deploying webstatic
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html> " > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+echo "
+events { }
+
+http {
+	server {
+    		listen 80;
+
+    	location / {
+        	root /var/www/html;
+		add_header X-Served-By $HOSTNAME;
+    	}
+
+        location /hbnb_static {
+            alias /data/web_static/current/;
+        }
+
+    	location /redirect_me {
+    		return 301 https://www.youtube.com/watch?v=6KFOcDmcoBg;
+	    }
+    	error_page 404 /custom_404.html;
+    	location = /custom_404.html {
+        	root /var/www/html;
+		internal;
+    	}
+	}
+}" > /etc/nginx/nginx.conf
+
+service nginx restart
