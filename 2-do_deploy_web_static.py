cc0c89c7fd2369a29141aed200bde9f9d92b5cc8
Francis Thore <thorefrancis@gmail.com>
2024-04-05 01:33:13 +0200

    okay partial

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Deploys an archive
 """
-from fabric.api import put, run
+from fabric.api import env, put, run, local
 import os
 
 env.hosts = ['3.84.239.148', '100.26.243.121']
@@ -9,16 +9,18 @@ env.hosts = ['3.84.239.148', '100.26.243.121']
 
 def do_deploy(archive_path):
     """Distributes an archive to webservers"""
-    if os.path.exists(archive_path):
-        archive, extention = os.path.splitext(archive_path)
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive = os.path.basename(archive_path).split('.')[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(archive))
-        run("tar -xzf /tmp/{}.tgz /data/web_static/releases/{}/".format(archive, archive))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(archive, archive))
         run("rm /tmp/{}.tgz".format(archive))
         run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(archive, archive))
         run("rm -rf /data/web_static/releases/{}/web_static".format(archive))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive))
         return True
-    else:
+    except:
         return False
