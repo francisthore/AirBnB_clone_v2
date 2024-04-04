de4f21215eda9b078937e3841b4dcba6db896aa2
Francis Thore <thorefrancis@gmail.com>
2024-04-04 14:23:09 -0700

    res now captured

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,18 @@
 to a tgz file
 """
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
     """Packages webstatic content
     for our deployment"""
-    date = "$(date '+%Y%m%d%H%M%S')"
-    local("mkdir versions")
-    local("tar -cvzf versions/web_static_{}.tgz web_static".format(date))
+    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_path = "versions/web_static_{}.tgz".format(date)
+    local("mkdir -p versions")
+    res = local("tar -cvzf {} web_static".format(archive_path), capture=True)
+
+    if res.succeeded:
+        return archive_path
+    else:
+        return None
