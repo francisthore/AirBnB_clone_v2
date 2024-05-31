e4e8aae2c43ce8357d52e7fba56baac8d096604c
Francis Thore <thorefrancis@gmail.com>
2024-05-31 22:46:55 +0200

    text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script that runs a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """HBNB route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun_route(text):
+    """Takes in a parameter"""
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
