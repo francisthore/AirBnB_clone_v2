79a5c6dfaa3ccb9888286f95be624f2d113acc95
Francis Thore <thorefrancis@gmail.com>
2024-06-24 21:50:09 +0200

    pass in as dict then use sort filter

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Makes db query and returns dict of states"""
-    states_l = sorted(list(storage.all('State').values()), key=lambda x: x.name)
+    states_l = storage.all(State)
     return render_template('7-states_list.html', states=states_l)
 
 
